d9289ad4cce4103e1b7d82dfa543e49940da65a6
gabri94 <gabrihacker@gmail.com>
2014-11-11 16:46:48 +0100

    code cleaned following pep8 removed old code, now test works!!!!
    corrected bug for py3
    
    maybe this time will work
    
    Corrected the difference function, now it return the difference between the edges of 2 graph.
    Made a new test for the parser.
    Fixed weight in json generation.

--- netdiff/olsr1.py
+++ netdiff/olsr1.py
@@ -10,7 +10,7 @@ class Olsr1Parser:
             self.parse(graph=self.old_graph, data=old)
         if new:
             self.parse(graph=self.new_graph, data=new)
-    
+
     def parse(self, graph, data):
         if type(data) is not dict:
             data = json.loads(data)
@@ -19,9 +19,17 @@ class Olsr1Parser:
                            link["destinationIP"],
                            weight=link["tcEdgeCost"])
         return graph
-       
+
     def diff(self):
+        def difference(old, new):
+            diff = old.copy()
+            diff.remove_edges_from(n for n in old.edges() if n in new.edges())
+            return diff
+
+
         return {
-            "added": networkx.difference(self.new_graph, self.old_graph),
-            "removed": networkx.difference(self.old_graph, self.new_graph)
+            "added": difference(old = self.new_graph,
+                                new = self.old_graph),
+            "removed": difference(old = self.old_graph,
+                                new = self.new_graph)
         }
--- tests/olsr1/tests.py
+++ tests/olsr1/tests.py
@@ -1,6 +1,9 @@
 import os
 import unittest
+import random
+import networkx as nx
 from netdiff.olsr1 import Olsr1Parser
+from .Olsr_graph_gen import OlsrGraphGen
 
 
 __all__ = ['TestOlsr1Parser']
@@ -11,9 +14,26 @@ topology1 = open('{0}/topology1.json'.format(CURRENT_DIR)).read()
 
 
 class TestOlsr1Parser(unittest.TestCase):
+    og = OlsrGraphGen(topology1)
     def test_nochanges(self):
         parser = Olsr1Parser(old=topology1, new=topology1)
         result = parser.diff()
         self.assertTrue(type(result) is dict)
         # TODO:
         # ensure added and removed have 0 changes
+
+    def test_different_leo(self):
+        for n in range(20,len(self.og.graph)):
+            parser = Olsr1Parser(old = topology1, new = self.og.gen_olsr_topo(self.og.reduce_graph(n)))
+            result = parser.diff()
+            self.assertFalse((len(result['removed'].edges())==0) and
+                             (len(result['added'].edges())==0))
+        self.assertTrue(1)
+
+    def small_graph_test(self):
+        old_graph = self.og.gen_olsr_topo(self.og.reduce_graph(10))
+        new_graph = self.og.gen_olsr_topo(self.og.reduce_graph(15))
+        parser = Olsr1Parser(old=old_graph, new = new_graph)
+        result = parser.diff();
+        self.assertTrue((len(result['removed'].edges())>0) or
+                         (len(result['added'].edges())>0))
