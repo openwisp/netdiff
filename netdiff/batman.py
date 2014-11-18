07614d43f0df9c51153cc86aa910ea85346bc92c
gabri94 <gabrihacker@gmail.com>
2014-11-18 13:51:56 +0100

    addedd batman parser, applyied DRY and created generic Parser

---
+++ netdiff/batman.py
@@ -0,0 +1,47 @@
+import json
+import networkx
+from netdiff.nxparser import Parser
+
+
+class BatmanParser(Parser):
+    """ Batman Topology Parser """
+    def _get_primary(self, mac, collection):
+        for node in collection:
+            for interface in node:
+                if mac == interface:
+                    return node[0]
+        return 0
+
+    def _get_ag_node_list(self, data):
+        agn = []
+        for node in data:
+            agi = []
+            agi.append(node['primary'])
+            if('secondary'in node):
+                for interface in node['secondary']:
+                    agi.append(interface)
+            agn.append(agi)
+        return agn
+
+    def _parse(self, data):
+        """
+        Converts a topology in a NetworkX MultiGraph object.
+
+        :param str topology: The OLSR1 topology to be converted (JSON or dict)
+        :return: the NetworkX MultiGraph object
+        """
+        # if data is not a python dict it must be a json string
+        if type(data) is not dict:
+            data = json.loads(data)
+        # initialize graph and list of aggregated nodes
+        graph = networkx.MultiGraph()
+        agn = self._get_ag_node_list(data['vis'])
+        # loop over topology section and create networkx graph
+        for node in data["vis"]:
+                for neigh in node["neighbors"]:
+                    p_neigh = self._get_primary(neigh['neighbor'], agn)
+                    if not graph.has_edge(node['primary'], p_neigh):
+                        graph.add_edge(node['primary'],
+                                       p_neigh,
+                                       weight=neigh['metric'])
+        return graph
