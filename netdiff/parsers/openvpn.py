4ebfb69cd6af931d78e49d7639804da66bfa858f
Federico Capoano <nemesis@ninux.org>
2017-09-22 12:31:49 +0200

    [openvpn] Simplified and improved OpenVPN parser

--- netdiff/parsers/openvpn.py
+++ netdiff/parsers/openvpn.py
@@ -1,36 +1,19 @@
 import networkx
-from openvpn_status import ParsingError, parse_status
+from openvpn_status import parse_status
 
-from ..exceptions import ConversionException, ParserError
 from .base import BaseParser
 
 
 class OpenvpnParser(BaseParser):
     """ OpenVPN status log parser """
-    protocol = 'openvpn'
-    server_address = '127.0.0.1' # Testing purposes
+    protocol = 'OpenVPN Status Log'
     version = '1'
-    metric = '0'
+    metric = 'static'
+    # for internal use only
+    _server_common_name = 'openvpn-server'
 
     def to_python(self, data):
-        status = parse_status(data)
-        nodes = []
-        for i in status.client_list:
-            node = status.client_list[i]
-            nodes.append({
-                'commonName': node.common_name,
-                'bytesSent': node.bytes_sent,
-                'bytesReceived': node.bytes_received,
-                'connectedSince': node.connected_since,
-                'realAddress': str(node.real_address)
-            })
-
-        return {
-            'type': 'OpenVPN',
-            'nodes': nodes,
-            'updated_at': status.updated_at,
-            'server': self.server_address
-        }
+        return parse_status(data)
 
     def parse(self, data):
         """
@@ -39,20 +22,24 @@ class OpenvpnParser(BaseParser):
         """
         # initialize graph and list of aggregated nodes
         graph = networkx.Graph()
-        if 'type' not in data or data['type'] != 'OpenVPN':
-            raise ParserError('Parse Error, not a OpenVPN object')
-        required_keys = ['nodes', 'server']
-        for key in required_keys:
-            if key not in data:
-                raise ParserError('Parse Error, "%s" key not found' % key)
-
-        graph.add_node(data['server'])
-        for link in data['nodes']:
-            try:
-                source = data['server']
-                target = link['realAddress']
-            except KeyError as e:
-                raise ParserError('Parse Error, "%s" key not found' % key)
-            graph.add_node(target)
-            graph.add_edge(source, target, weight=1)
+        server = self._server_common_name
+        # add server (central node) to graph
+        graph.add_node(server)
+        # add clients in graph as nodes
+        for common_name, client in data.client_list.items():
+            client_properties = {
+                'real_address': str(client.real_address.host),
+                'port': client.real_address.port,
+                'connected_since': client.connected_since.strftime('%Y-%m-%dT%H:%M:%SZ'),
+                'bytes_received': client.bytes_received,
+                'bytes_sent': client.bytes_sent
+            }
+            if common_name in data.routing_table:
+                client_properties['local_addresses'] = [
+                    str(data.routing_table.get(common_name).virtual_address)
+                ]
+            graph.add_node(common_name, **client_properties)
+        # add links in routing table to graph
+        for common_name, link in data.routing_table.items():
+            graph.add_edge(server, common_name, weight=1)
         return graph
