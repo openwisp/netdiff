d69519214d48a80b9c8f66da12830f99d5cc506b
Rohith Asrk <rohith.asrk@gmail.com>
2017-10-22 21:53:58 +0530

    Added support to TAP mode in OpenVPN parser

--- tests/test_openvpn.py
+++ tests/test_openvpn.py
@@ -3,11 +3,11 @@ import os
 import networkx
 import six
 from netdiff import OpenvpnParser
-from netdiff.exceptions import ParserError, TopologyRetrievalError
 from netdiff.tests import TestCase
 
 CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
 links2 = open('{0}/static/openvpn-2-links.txt'.format(CURRENT_DIR)).read()
+links2_tap = open('{0}/static/openvpn-5-links-tap.txt'.format(CURRENT_DIR)).read()
 
 
 class TestOpenvpnParser(TestCase):
@@ -31,3 +31,17 @@ class TestOpenvpnParser(TestCase):
         self.assertIsInstance(data['links'], list)
         self.assertEqual(len(data['nodes']), 3)
         self.assertEqual(len(data['links']), 2)
+
+    def test_json_dict_tap(self):
+        p = OpenvpnParser(links2_tap)
+        data = p.json(dict=True)
+        self.assertIsInstance(data, dict)
+        self.assertEqual(data['type'], 'NetworkGraph')
+        self.assertEqual(data['protocol'], 'OpenVPN Status Log')
+        self.assertEqual(data['version'], '1')
+        self.assertEqual(data['revision'], None)
+        self.assertEqual(data['metric'], 'static')
+        self.assertIsInstance(data['nodes'], list)
+        self.assertIsInstance(data['links'], list)
+        self.assertEqual(len(data['nodes']), 6)
+        self.assertEqual(len(data['links']), 5)
