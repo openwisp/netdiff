5e997375b1ffa19d1ad66a75df0bc52bd3615d51
Bhavya Muni <bhavyamuni@outlook>
2019-12-03 13:49:49 +0530

    [dependencies] Update networkx version #73

--- setup.py
+++ setup.py
@@ -34,10 +34,12 @@ def get_install_requires():
         # skip to next iteration if comment or empty line
         if line.startswith('#') or line == '' or line.startswith('http') or line.startswith('git'):
             continue
-        if line.startswith("networkx") and sys.version_info.major < 3:
-            line.replace("<", "=")
         # add line to requirements
         requirements.append(line.replace('\n', ''))
+    if sys.version_info.major < 3:
+        requirements.append("networkx==2.2")
+    else:
+        requirements.append("networkx>=2.2")
     return requirements
 
 
