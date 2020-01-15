29d97f0ba1d63db8fb7ef5783249ab87f4b7c1de
Federico Capoano <federico.capoano@gmail.com>
2020-01-15 12:13:26 +0100

    [fix] Fixed setup.py get_install_requires()

--- setup.py
+++ setup.py
@@ -34,6 +34,8 @@ def get_install_requires():
         # skip to next iteration if comment or empty line
         if line.startswith('#') or line == '' or line.startswith('http') or line.startswith('git'):
             continue
+        # add line to requirements
+        requirements.append(line.replace('\n', ''))
     return requirements
 
 
