e7499628d3bd904ce185a5040d1873563e520a0a
Federico Capoano <f.capoano@openwisp.io>
2023-11-24 15:42:06 -0300

    [fix] Fixed get_version compliance

--- netdiff/info.py
+++ netdiff/info.py
@@ -9,9 +9,10 @@ def get_version():
     if VERSION[3:] == ('alpha', 0):
         version = '%s pre-alpha' % version
     else:
-        try:
-            rev = VERSION[4]
-        except IndexError:
-            rev = 0
-        version = '%s%s%s' % (version, VERSION[3][0:1], rev)
+        if VERSION[3] != 'final':
+            try:
+                rev = VERSION[4]
+            except IndexError:
+                rev = 0
+            version = '%s%s%s' % (version, VERSION[3][0:1], rev)
     return version
